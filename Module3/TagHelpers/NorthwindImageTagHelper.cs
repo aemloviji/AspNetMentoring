011454604b5bf8b38f2913ebfd8926131aba5e0e
Elvin Asadov <aemloviji@gmail.com>
2019-04-05 19:26:27 +0300

    Module3.Task3.Part2 Finished Added NorthwindImageTagHelper to produce custom link for category image by given id

---
+++ Module3/TagHelpers/NorthwindImageTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace Module3.TagHelpers
+{
+    [HtmlTargetElement(Attributes = "northwind-id")]
+    public class NorthwindImageTagHelper : TagHelper
+    {
+        public string NorthwindId { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var childContent = await output.GetChildContentAsync();
+
+            output.Attributes.SetAttribute("href", $"/images/{NorthwindId}");
+            output.Content.SetContent(childContent.GetContent());
+        }
+    }
+}
