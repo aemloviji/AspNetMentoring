037e5d26b0abb0120f5cc069d110b88203a44689
AEMLoviji <AEMLoviji@gmail.com>
2019-05-14 23:42:59 +0300

    Module5 Task3 - added AdministratorController

---
+++ Module5/Data/SeedData.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace Module5.Data
+{
+    public class SeedData
+    {
+        private const string AdminRole = "administrator";
+        public static async Task Initialize(IServiceProvider serviceProvider, string adminUserName, string adminPwd)
+        {
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            {
+                await CreateRole(serviceProvider, AdminRole);
+                var adminID = await CreateAdmin(serviceProvider, adminPwd, adminUserName);
+                await GrantAdminRoleToAdminUser(serviceProvider, adminID, AdminRole);
+            }
+        }
+
+        private static async Task CreateRole(IServiceProvider serviceProvider, string role)
+        {
+            var roleManager = serviceProvider.GetService<RoleManager<IdentityRole>>();
+            if (roleManager == null)
+            {
+                throw new Exception("roleManager is null");
+            }
+
+            if (!await roleManager.RoleExistsAsync(role))
+            {
+                await roleManager.CreateAsync(new IdentityRole(role));
+            }
+        }
+
+
+        private static async Task<string> CreateAdmin(IServiceProvider serviceProvider,
+                                                        string adminPwd, string UserName)
+        {
+            var userManager = serviceProvider.GetService<UserManager<IdentityUser>>();
+
+            var user = await userManager.FindByNameAsync(UserName);
+            if (user == null)
+            {
+                user = new IdentityUser { UserName = UserName };
+                await userManager.CreateAsync(user, adminPwd);
+            }
+
+            return user.Id;
+        }
+
+        private static async Task<IdentityResult> GrantAdminRoleToAdminUser(IServiceProvider serviceProvider, string uid, string role)
+        {
+            var userManager = serviceProvider.GetService<UserManager<IdentityUser>>();
+            var user = await userManager.FindByIdAsync(uid);
+            if (user == null)
+            {
+                throw new Exception("User not exists!");
+            }
+
+            return await userManager.AddToRoleAsync(user, role);
+        }
+    }
+}
--- Module5/Program.cs
+++ Module5/Program.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Module5.Data;
 
 namespace Module5
 {
@@ -14,7 +17,30 @@ namespace Module5
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    context.Database.Migrate();
+
+                    var config = host.Services.GetRequiredService<IConfiguration>();
+                    var adminUserPassword = config["AdminUserPwd"];
+
+                    SeedData.Initialize(services, "admin@module.net", adminUserPassword).Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
