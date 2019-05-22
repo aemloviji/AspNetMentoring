61d0535eee25259bea08caf49450bb5827b3975f
Elvin Asadov <EAsadov2@slb.com>
2019-05-22 21:23:27 +0300

    Module 6 Task1 IIS Profile added

---
+++ Module6.Task1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Module6.Task1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
